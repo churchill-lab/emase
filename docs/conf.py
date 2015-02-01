fb3224f67dcee2713b06b2496aebe345719f05cf
KB Choi <kwangbom@gmail.com>
2015-02-01 17:30:55 -0500

    [kbchoi] Trying to fix documentation error.

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,15 @@
 
 import sys
 import os
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'scipy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
@@ -30,6 +39,7 @@ project_root = os.path.dirname(cwd)
 # This lets us ensure that the source package is imported, and that its
 # version is used.
 sys.path.insert(0, project_root)
+
 
 import emase
 
