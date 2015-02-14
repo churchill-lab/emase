d7927f7ec5a95bcc10ebe2e48aa4227dab7e0e1a
KB Choi <kwangbom@gmail.com>
2015-02-14 15:39:39 -0500

    Implementing new normalization models

--- emase/EMfactory.py
+++ emase/EMfactory.py
@@ -60,6 +60,23 @@ class EMfactory:
             self.allelic_expression[:, nzloci] += pseudocount
             self.allelic_expression *= (orig_allelic_expression_sum / self.allelic_expression.sum())  # original depth scale
 
+    def reset(self, pseudocount=0.0):
+        """
+        Initialize the probability of read origin according to the alignment profile
+        :param pseudocount: Uniform prior for allele specificity estimation
+        :return: Nothing (as it performs an in-place operations)
+        """
+        self.probability.reset()
+        self.probability.normalize_reads(axis=APM.Axis.READ)  # Initialize alignment probability matrix
+        self.allelic_expression = self.probability.sum(axis=APM.Axis.READ)
+        if self.target_lengths is not None:  # allelic_expression will be at depth-level
+            self.allelic_expression = np.divide(self.allelic_expression, self.target_lengths)
+        if pseudocount > 0.0:  # pseudocount is at depth-level
+            orig_allelic_expression_sum = self.allelic_expression.sum()
+            nzloci = np.nonzero(self.allelic_expression)[1]
+            self.allelic_expression[:, nzloci] += pseudocount
+            self.allelic_expression *= (orig_allelic_expression_sum / self.allelic_expression.sum())  # original depth scale
+
     def get_allelic_expression(self, at_group_level=False):
         if at_group_level:
             return self.allelic_expression * self.grp_conv_mat
