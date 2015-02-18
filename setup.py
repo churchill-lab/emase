c43fbe632ff655a0e71f62da2b3bf44c55fe0f71
KB Choi <kwangbom@gmail.com>
2015-02-18 10:29:23 -0500

    Fixed requirements.txt file for Travis CI build to succeed

--- setup.py
+++ setup.py
@@ -16,11 +16,11 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
+    'numexpr=2.3.1',
     'pysam>=0.6',
-    'numexpr>=2.1',
-    'numpy>=1.7,<1.9',
-    'scipy>=0.12,<0.14',
-    'tables>=3.0,<3.1.1'
+    'numpy=1.8.2',
+    'scipy=0.13.3',
+    'tables=3.1.0'
 ]
 
 test_requirements = [
