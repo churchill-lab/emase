Numerical issue (NaN) was fixed

emase/AlignmentPropertyMatrix.py
@@ -211,8 +211,9 @@ class AlignmentPropertyMatrix(Sparse3DMatrix):
         if self.finalized:
             if axis == self.Axis.LOCUS:  # Locus-wise normalization on each read
                 normalizer = self.sum(axis=self.Axis.HAPLOTYPE)  # Sparse matrix of |reads| x |loci|
-                normalizer.data += 0.000001  # Trying to avoid nan problem
+                normalizer.eliminate_zeros()
                 for hid in xrange(self.num_haplotypes):
+                    self.data[hid].eliminate_zeros()  # Trying to avoid numerical problem (inf or nan)
                     self.data[hid] = np.divide(self.data[hid], normalizer)  # element-wise division
             elif axis == self.Axis.HAPLOTYPE:  # haplotype-wise normalization on each read
                 for hid in xrange(self.num_haplotypes):
@@ -232,15 +233,15 @@ class AlignmentPropertyMatrix(Sparse3DMatrix):
                 # for hid in xrange(self.num_haplotypes):
                 #     self.data[hid] = self.data[hid] / normalizer.transpose()
                 normalizer = self.sum(axis=self.Axis.HAPLOTYPE) * grouping_mat
-                normalizer.data += 0.000001  # Trying to avoid nan problem
                 for hid in xrange(self.num_haplotypes):
+                    self.data[hid].eliminate_zeros()  # Trying to avoid numerical problem (inf or nan)
                     self.data[hid] = np.divide(self.data[hid], normalizer)
             elif axis == self.Axis.HAPLOGROUP:  # haplotype-wise & group-wise normalization on each read
                 if grouping_mat is None:
                     raise RuntimeError('Group information matrix is missing.')
                 for hid in xrange(self.num_haplotypes):  # normalizer is different hap-by-hap
                     normalizer = self.data[hid] * grouping_mat  # Sparse matrix of |reads| x |loci|
-                    normalizer.data += 0.000001  # Trying to avoid nan problem
+                    self.data[hid].eliminate_zeros()  # Trying to avoid numerical problem (inf or nan)
                     self.data[hid] = np.divide(self.data[hid], normalizer)
             else:
                 raise RuntimeError('The axis should be 0, 1, 2, or 3.')

emase/EMfactory.py
@@ -128,8 +128,6 @@ class EMfactory:
         :param model: Normalization model (1: Gene->Isoform->Allele, 2: Gene->Allele->Isoform, 3: Gene->Isoform*Allele, 4: RSEM)
         :return: Nothing (as it performs in-place operations)
         """
-        err_states = np.seterr(all='warn')
-        err_states = np.seterr(**err_states)
         self.update_probability_at_read_level(model)
         self.allelic_expression = self.probability.sum(axis=APM.Axis.READ)
         if self.target_lengths is not None:
@@ -161,6 +159,8 @@ class EMfactory:
         :param verbose: Display information on how EM is running
         :return: Nothing (as it performs in-place operations)
         """
+        orig_err_states = np.seterr(all='raise')
+        np.seterr(under='ignore')
         if verbose:
             print
             print "Iter No  Time (hh:mm:ss)  Total error (depth)  Max error (%)  Locus of max error  Allele expression change"