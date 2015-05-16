[kbchoi] Fixed length normalization so it now uses (L-k+1) where L is target length and k is read length.

emase/EMfactory.py
@@ -47,8 +47,8 @@ class EMfactory:
                 for curline in fh:
                     item = curline.rstrip().split("\t")
                     locus, hap = item[0].split("_")
-                    self.target_lengths[self.probability.lid[locus], hid[hap]] = max(float(item[1]), 1.0)
-            self.target_lengths = self.target_lengths.transpose() / read_length  # lengths in terms of read counts
+                    self.target_lengths[self.probability.lid[locus], hid[hap]] = max(float(item[1]) - read_length + 1.0, 1.0)
+            #self.target_lengths = self.target_lengths.transpose() / read_length  # lengths in terms of read counts
             if not np.all(self.target_lengths > 0.0):
                 raise RuntimeError('There exist transcripts missing length information.')
         self.probability.normalize_reads(axis=APM.Axis.READ)  # Initialize alignment probability matrix