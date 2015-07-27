2c74d242d694418b38899f1adcfc44a6411a357c
[kbchoi] <kb.choi@jax.org>
2015-07-27 13:31:49 -0400

    [kbchoi] Implementing simulate-reads (Model 1 is done)

--- scripts/simulate-reads.py
+++ scripts/simulate-reads.py
@@ -13,7 +13,8 @@ __author__ = 'Kwangbom "KB" Choi, Ph. D.'
 
 help_message = '''
     Usage:
-        simulate-reads -F <target_fasta> -p <param_file> -m <simulation_model> -N <total_count> [ -r <read_len> -e <err_rate> ]
+        simulate-reads -R <reference_dir> -H <hybrid_dir> -p <param_file> -m <simulation_model> -N <total_count> \\
+                     [ -r <read_len> -e <err_rate> ]
 
     Input:
         <target_fasta> : A target fasta file
@@ -41,14 +42,16 @@ def main(argv=None):
         argv = sys.argv
     try:
         try:
-            opts, args = getopt.getopt(argv[1:], "hF:g:p:m:N:r:e:", \
-                                       ["help", "grouping-file", "parameter-file", "total-count", "error-rate"])
+            opts, args = getopt.getopt(argv[1:], "hR:H:p:m:N:r:e:", \
+                                       ["help", "ref-dir", "hybrid-dir", "parameter-file", "total-count", "error-rate"])
         except getopt.error, msg:
             raise Usage(msg)
 
         # Default values of vars
+        ref_dir = os.getenv('EMASE_INDEX_BASE', '.')
+        hyb_dir = '.'
         read_len = 100
-        lamb = 0.1
+        error_rate = 0.1
         mut = {'A':('T', 'G', 'C'), \
                'T':('G', 'C', 'A'), \
                'G':('C', 'A', 'T'), \
@@ -58,34 +61,53 @@ def main(argv=None):
         for option, value in opts:
             if option in ("-h", "--help"):
                 raise Usage(help_message)
-            if option == "-F":
-                target_file = value
-            if option in ("-g", "--grouping-file"):
-                grouping_file = value
+            if option in ("-R", "--ref-dir"):
+                ref_dir = value
+            if option in ("-H", "--hybrid-dir"):
+                hyb_dir = value
             if option in ("-p", "--parameter-file"):
                 param_file = value
             if option == "-m":
                 model = int(value)
             if option in ("-N", "--total-count"):
-                model = int(value)
+                total_simreads = int(value)
             if option == "-r":
-                model = int(value)
+                read_len = int(value)
             if option in ("-e", "--error-rate"):
-                model = int(value)
+                error_rate = int(value)
             if option == "-o":
                 outfile = value
 
-        # Check if the required options are given
+        # Check if the required options are all given
+        grouping_file = os.path.join(ref_dir, 'emase.gene2transcripts.tsv')
+        if not os.path.exists(grouping_file):
+            print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + '%s does not exist at %d.' % (grouping_file, ref_dir)
+            return 2
+        target_file = os.path.join(hyb_dir, 'emase.pooled.transcripts.fa')
+        if not os.path.exists(target_file):
+            print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + '%s does not exist at %d.' % (target_file, hyb_dir)
+            return 2
+        target_info_file = os.path.join(hyb_dir, 'emase.pooled.transcripts.info')
+        if not os.path.exists(target_info_file):
+            print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + '%s does not exist at %d.' % (target_info_file, hyb_dir)
+            return 2
         if model not in (1, 2, 3, 4):
             print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + 'Simulation model should be either 1, 2, 3 or 4.'
             return 2
+        outfile = './emase.M%d.simulated.fa' % model
 
 
         #
         # Main body
         #
 
-        gname  = list()
+        #
+        # Load parameters and other required information
+        tname = np.loadtxt(target_info_file, dtype='string', usecols=(0,))
+        num_transcripts = len(tname)
+        tid = dict(zip(tname, np.arange(num_transcripts)))
+
+        gname = list()
         groups = list()
         with open(grouping_file) as fh:
             for curline in fh:
@@ -94,15 +116,14 @@ def main(argv=None):
                 tid_list = [ tid[t] for t in item[1:] ]
                 groups.append(tid_list)
         gname = np.array(gname)
-        gid = dict(zip(gname, np.arange(len(gname))))
-        grp_conv_mat = lil_matrix((len(tname), len(gname)))
-        for i in xrange(len(gname)):
+        num_genes = len(gname)
+        gid = dict(zip(gname, np.arange(num_genes)))
+        grp_conv_mat = lil_matrix((num_transcripts, num_genes))
+        for i in xrange(num_genes):
             grp_conv_mat[groups[i], i] = 1.0
         grp_conv_mat = grp_conv_mat.tocsc()
 
-        # Generate read counts
-
-        emase_count_transcript = np.zeros((len(tname), 2))
+        tacount = np.zeros((num_transcripts, 2))
         with open(param_file) as fh:
             curline = fh.next()
             item = curline.rstrip().split('\t')
@@ -111,27 +132,37 @@ def main(argv=None):
             hid = dict(zip(hname, np.arange(num_haps)))
             for curline in fh:
                 item = curline.rstrip().split('\t')
-                emase_count_transcript[tid[item[0]], :] = map(float, item[1:3])
-        emase_count_transcript.shape, emase_count_transcript.sum()
+                tacount[tid[item[0]], :] = map(float, item[1:(1+num_haps)])
+        tacount.shape, tacount.sum()
 
+        trange = np.zeros(tacount.shape)
+        with open(target_info_file) as fh:
+            for curline in fh:
+                item = curline.rstrip().split('\t')
+                t, h = item[0].split('_')
+                trange[tid[t], hid[h]] = np.int(item[1])
+        trange = trange - read_len + 1
+
+        #
+        # Generate read counts
         if model == 1:
-            gacount = grp_conv_mat.transpose() * emase_count_transcript
+            gacount = grp_conv_mat.transpose() * tacount
             gcount = gacount.sum(axis=1)
             theta = gcount / gcount.sum()
-            gexpr = gcount > 0
-            gcount_sim = multinomial(N, theta)[0]
+            gcount_sim = multinomial(total_simreads, theta)[0]
             phi = np.zeros(gacount.shape)
+            gexpr = gcount > 0
             phi[gexpr, :] = gacount[gexpr, :] / gcount[gexpr, np.newaxis]
             phi[np.logical_not(gexpr), :] = np.ones(num_haps) / num_haps
             gacount_sim = np.zeros(gacount.shape)
             for g in xrange(num_genes):
                 gacount_sim[g] = multinomial(gcount_sim[g], phi[g])
-            tacount_sim = np.zeros(emase_count_transcript.shape)
+            tacount_sim = np.zeros(tacount.shape)
             for g in xrange(num_genes):
                 tindex = groups[g]
                 num_isoforms = len(tindex)
                 for h in xrange(num_haps):
-                    delta = emase_count_transcript[tindex, h]
+                    delta = tacount[tindex, h]
                     delta_sum = delta.sum()
                     if delta_sum > 0:
                         delta /= delta_sum
@@ -142,50 +173,40 @@ def main(argv=None):
             pass
         elif model == 3:
             pass
-        else:  # if model == 4:
+        else:  # elif model == 4:
             pass
 
+        out_dir = os.path.dirname(outfile)
+        true_gaout = os.path.join(out_dir, 'emase.M%d.simulated.genes.read_counts' % model)
+        np.savetxt(true_gaout, np.column_stack((gname, np.char.mod('%d', gacount_sim))), \
+                   header='Gene_ID\t' + "\t".join(hname), fmt='%s', delimiter='\t')
+        true_taout = os.path.join(out_dir, 'emase.M%d.simulated.transcripts.read_counts' % model)
+        np.savetxt(true_taout, np.column_stack((tname, np.char.mod('%d', tacount_sim))), \
+                   header='Transcript_ID\t' + "\t".join(hname), fmt='%s', delimiter='\t')
 
+        #
         # Generate reads
-
         f = pysam.FastaFile(target_file)
-        target_info_file = os.path.splitext(target_file)[0] + '.info'
-        trange = np.zeros(emase_count_transcript.shape)
-        with open(target_info_file) as fh:
-            for curline in fh:
-                item = curline.rstrip().split('\t')
-                t, h = item[0].split('_')
-                trange[tid[t], hid[h]] = np.int(item[1])
-        trange = trange - read_len + 1
-
         with open(outfile, 'w') as fhout:
-            with open('emase.M1.simulated.transcripts.read_counts') as fh:
-                curline = fh.next()
-                item = curline.rstrip().split('\t')
-                hname = item[1:]
-                hid = dict(zip(hname, np.arange(len(hname))))
-                for curline in fh:
-                    item = curline.rstrip().split('\t')
-                    t = item[0]
-                    hcount = map(int, item[1:])
-                    for h in xrange(num_haps):
-                        th = '%s_%s' % (t, hname[h])
-                        trange_max = trange[tid[t], h]
-                        for k in xrange(hcount[h]):
-                            start = randint(0, trange_max)
-                            end = start + read_len
-                            seq = f.fetch(th, start, end)
-                            nerr = poisson(lamb)
-                            errstr = ''
-                            if nerr > 0:
-                                errloc = sorted(choice(read_len, nerr, replace=False))
-                                errstr = ':'
-                                for epos in errloc:
-                                    from_base = seq[epos]
-                                    to_base = mut[from_base][randint(3)]
-                                    seq = introduce_mutation(seq, epos, to_base)
-                                    errstr += '[%d]%s->%s;' % (epos+1, from_base, to_base)
-                            fhout.write(">%s:%d-%d%s\n%s\n" % (th, start+1, end, errstr, seq))
+            for t in xrange(num_transcripts):
+                for h in xrange(num_haps):
+                    theader = '%s_%s' % (tname[t], hname[h])
+                    trange_max = trange[t, h]
+                    for cnt in xrange(tacount_sim[t, h]):
+                        start = randint(0, trange_max)
+                        end = start + read_len
+                        seq = f.fetch(theader, start, end)
+                        nerr = poisson(error_rate)
+                        errstr = ''
+                        if nerr > 0:
+                            errloc = sorted(choice(read_len, nerr, replace=False))
+                            errstr = ':'
+                            for epos in errloc:
+                                from_base = seq[epos]
+                                to_base = mut[from_base][randint(3)]
+                                seq = introduce_mutation(seq, epos, to_base)
+                                errstr += '[%d]%s->%s;' % (epos+1, from_base, to_base)
+                        fhout.write(">%s:%d-%d%s\n%s\n" % (theader, start+1, end, errstr, seq))
 
         #
         # End of Main body
