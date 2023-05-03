56ee8d19b88cadc2ab7632ff291eeb0a428f52d5
Mike Lloyd <mike.lloyd@jax.org>
2023-05-03 11:24:12 -0400

    added init

---
+++ emase/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .Sparse3DMatrix import Sparse3DMatrix
+from .AlignmentPropertyMatrix import AlignmentPropertyMatrix
+from .AlignmentMatrixFactory import AlignmentMatrixFactory
+from .EMfactory import EMfactory
+
+__author__ = 'Kwangbom "KB" Choi, Ph. D., The Jackson Laboratory'
+__email__ = ''
+__version__ = '0.11.00'
